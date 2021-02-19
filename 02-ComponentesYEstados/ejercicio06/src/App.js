5329c912355ac0c48250611fb187545c80d4f71b
Santoxjon <jonsantox@gmail.com>
2021-02-19 01:19:31 +0100

    Ejercicio 6 terminado

--- 02-ComponentesYEstados/ejercicio06/src/App.js
+++ 02-ComponentesYEstados/ejercicio06/src/App.js
@@ -8,7 +8,7 @@ const libreria = {
     nombre: "Javier",
     vip: false,
     imagen:
-      "https://img.freepik.com/free-vector/businessman-profile-cartoon_18591-58479.jpg",
+      "https://img.freepik.com/free-vector/businessman-profile-cartoon_18591-58479.jpg"
   },
   libros: [
     {
@@ -18,8 +18,8 @@ const libreria = {
       editorial: "Ediciones B / Zeta",
       descripcion:
         "La Tierra está amenazada por una especie extraterrestre de insectos quepretende destruir la humanidad. Para vencerlos se precisa la intervención de un genio militar,por lo cual se permite el nacimiento de Ender, tercer hijo de una pareja en un mundo que limitaa dos el número de descendientes. Ender se entrenará en una estación espacial, superará asus rivales y se convertirá en la persona capaz de dirigir las flotas terrestres contra los insectosde otros mundos.",
-      img: "​/images/image1.jpg",
-      stock: 25,
+      img: "https://i.pinimg.com/474x/1e/6c/50/1e6c50e97899bbfed4fa294dc6f64223.jpg",
+      stock: 25
     },
     {
       id: "2",
@@ -28,9 +28,8 @@ const libreria = {
       editorial: "Gigamesh",
       descripcion:
         "Tras el largo verano, el invierno se acerca a los Siete Reinos. Lord EddardStark, señor de Invernalia, deja sus dominios para unirse a la corte de su amigo el rey RobertBaratheon, llamado el Usurpador, hombre díscolo y otrora guerrero audaz cuyas mayoresaficiones son comer, beber y engendrar bastardos. Eddard Stark ocupará el cargo de Mano delRey e intentará desentrañar una maraña de intrigas que pondrá en peligro su vida y la de todoslos suyos. En un mundo cuyas estaciones pueden durar decenios y en el que retazos de unamagia inmemorial y olvidada surgen en los rincones más sombrios y maravillosos, la traición yla lealtad, la compasión y la sed de venganza, el amor y el poder hacen del juego de tronos unapoderosa trampa que atrapará en sus fauces a los personajes... y al lector.",
-      img:
-        "​/images/image2.jpg",
-      stock: 2,
+      img: "https://www.laluilolo.com/wp-content/uploads/2019/09/sport-water-eng-garnet-350x350.jpg",
+      stock: 2
     },
     {
       id: "3",
@@ -39,43 +38,44 @@ const libreria = {
       editorial: "Edhasa",
       descripcion:
         "Los robots de Isaac Asimov son máquinas capaces de llevar a cabo muydiversas tareas, y que a menudo se plantean a sí mismos problemas de 'conducta humana'.Pero estas cuestiones se resuelven en Yo, robot en el ámbito de las tres leyes fundamentalesde la robótica, concebidas por Asimov, y que no dejan de proponer extraordinarias paradojasque a veces se explican por errores de funcionamiento y otras por la creciente complejidad delos 'programas'. Las paradojas que se plantean en estos relatos futuristas no son sóloingeniosos ejercicios intelectuales sino sobre todo una indagación sobre la situación delhombre actual en relación con los avances tecnológicos y con la experiencia del tiempo.",
-      img: "/images/image3.jpg",
-      stock: 10,
-    },
+      img: "http://frutasfercas.com/wp-content/uploads/2018/03/limon-1-350x350.jpg",
+      stock: 10
+    }
   ],
 };
 
 function App() {
   let [vip, setVip] = useState(libreria.usuario.vip);
   let [greetings, setGreetings] = useState("");
+  let randomNumber = Math.floor(Math.random() * libreria.libros.length);
 
   let books = libreria.libros;
-  let bookList = books.map((book) =>
-    <Book title={book.titulo} author={book.autor} description={book.descripcion} image={book.img} stock={book.stock} />
+  let bookList = books.map((book, index) =>
+    <Book key={book.id} id={book.id} title={book.titulo} author={book.autor} description={book.descripcion} image={book.img} stock={book.stock} index={index} libreria={libreria} />
   );
 
   function toVip() {
-    if (vip) {
-      setVip(false);
-      setGreetings("")
-    }
-    else {
-      setVip(true);
-      setGreetings("Bienvenido, ")
-    }
+    libreria.usuario.vip = vip ? false : true;
+    setVip(libreria.usuario.vip);
+    setGreetings(libreria.usuario.vip ? "Bienvenido de nuevo, " : "");
   }
   return (
     <div className="App">
 
       <header>
-        <h1>{greetings}{libreria.usuario.nombre}</h1>
-        <img src={libreria.usuario.imagen} />
+        <div className="left-header">
+          <img src={libreria.usuario.imagen} />
+          <h1>{greetings}{libreria.usuario.nombre}</h1>
+        </div>
         <button onClick={toVip}>Cambiar rango!</button>
       </header>
       <main>
         {bookList}
       </main>
-      <footer></footer>
+      <footer>
+        <h3>⬐ ¡ Libro de OFERTA ! ⬎</h3>
+        <p>{libreria.libros[randomNumber].titulo} - {libreria.libros[randomNumber].autor}</p>
+      </footer>
     </div>
   );
 }
--- 02-ComponentesYEstados/ejercicio06/src/Book.js
+++ 02-ComponentesYEstados/ejercicio06/src/Book.js
@@ -1,13 +1,37 @@
+import React, { useState } from 'react';
+
 function Book(props) {
+    let [stock, setStock] = useState(props.libreria.libros[props.index].stock);
+
+    function substractStock() {
+        if (stock > 1) {
+            setStock(+props.libreria.libros[props.index].stock - 1);
+        }
+        else {
+            setStock("No quedan copias");
+            document.getElementById(`btn_${props.id}`).disabled = true;
+        }
+        props.libreria.libros[props.index].stock = +props.libreria.libros[props.index].stock - 1;
+    }
+
     return (
         <div className="book">
-            <h1>{props.title}</h1>
-            <h2>{props.author}</h2>
-            <p>{props.description}</p>
-            <img src={props.image} />
-            <p>{props.stock}</p>
-        </div>
+            <div className="left">
+                <h1>{props.libreria.libros[props.index].titulo}</h1>
+                <h2>{props.libreria.libros[props.index].autor}</h2>
+                <hr />
+                <p>{props.libreria.libros[props.index].descripcion}</p>
+                <hr />
+                <div className="stock">
+                    <p>{stock}</p>
+                    <button onClick={substractStock} id={"btn_" + props.id}>Click me!</button>
+                </div>
+            </div>
+            <div className="right">
+                <img src={props.libreria.libros[props.index].img} />
+            </div>
+        </div >
     );
 }
 
-export default Book
+export default Book;
