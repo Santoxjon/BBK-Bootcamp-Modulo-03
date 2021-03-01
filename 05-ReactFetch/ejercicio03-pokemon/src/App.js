76940dd2bd210b1cefd7f6a35a0d563de8934806
Jon Santos <jonsantox@gmail.com>
2021-03-01 13:13:02 +0100

    Ejercicio 3 terminado

--- 05-ReactFetch/ejercicio03-pokemon/src/App.js
+++ 05-ReactFetch/ejercicio03-pokemon/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const URL_types = "https://pokeapi.co/api/v2/type/";
 
 function App() {
+  const [types, setTypes] = useState();
+  const [pkm1, setPkm1] = useState();
+  const [pkm2, setPkm2] = useState();
+  const [pkm3, setPkm3] = useState();
+  const [selectedOption, setSelectedOption] = useState("")
+
+  useEffect(() => {
+    fetch(URL_types)
+      .then(res => res.json())
+      .then(data => {
+        setTypes(data.results.map(type => {
+          return (
+            <option value={type.url}>{type.name}</option>
+          )
+        }));
+      });
+  }, [])
+
+  function typeSelected(event) {
+    setSelectedOption(event.target.value);
+    fetch(event.target.value)
+      .then(res => res.json())
+      .then(data => {
+        let max = data.pokemon.length;
+
+        let rand1 = ~~(Math.random() * max);
+        let rand2 = ~~(Math.random() * max);
+        let rand3 = ~~(Math.random() * max);
+
+        setPkm1(data.pokemon[rand1].pokemon.name);
+        setPkm2(data.pokemon[rand2].pokemon.name);
+        setPkm3(data.pokemon[rand3].pokemon.name);
+
+      })
+  }
+
+  function Pokemon(params) {
+    return (<p> {params.pokemon} </p>)
+  }
+
+  function Tipos() {
+    return (
+      <select onChange={typeSelected} value={selectedOption}>
+        <option hidden></option>
+        {types}
+      </select>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tipos />
+      <Pokemon pokemon={pkm1} />
+      <Pokemon pokemon={pkm2} />
+      <Pokemon pokemon={pkm3} />
     </div>
   );
 }
