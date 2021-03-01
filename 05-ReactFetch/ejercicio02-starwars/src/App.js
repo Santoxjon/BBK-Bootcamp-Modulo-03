691e37b1729327d6342d43c322e0225dc020388b
Jon Santos <jonsantox@gmail.com>
2021-03-01 12:47:21 +0100

    Nos han funado de la API de SW

--- 05-ReactFetch/ejercicio02-starwars/src/App.js
+++ 05-ReactFetch/ejercicio02-starwars/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
+const URL = "https://swapi.dev/api/planets/";
 function App() {
+
+  const [planets, setPlanets] = useState();
+  const [selectedPlanet, setSelectedPlanet] = useState("");
+  const [planetURL, setPlanetURL] = useState("");
+  const [residentsURL, setResidentsURL] = useState();
+  const [resNames, setResNames] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        setPlanets(data.results.map((planet) => {
+          return (
+            <option data-url={planet.url} value={planet.name}>{planet.name}</option>
+          )
+        }));
+      }
+      );
+  }, [])
+
+  useEffect(() => {
+    if (planetURL !== "") {
+      fetch(planetURL)
+        .then(res => res.json())
+        .then(data => {
+          setResidentsURL(data.residents);
+        });
+    }
+  }, [planetURL])
+
+  useEffect(() => {
+    if (planetURL !== "") {
+      let array = [];
+      residentsURL.forEach(resident_url => {
+        fetch(resident_url)
+          .then(res => res.json())
+          .then(data => {
+            array.push(data.name);
+          });
+      });
+      console.log(array);
+      setResNames(array);
+    }
+  }, [residentsURL])
+
+  function planetChanged(e) {
+    console.log("Hay cambio");
+    setSelectedPlanet(e.target.value)
+    setPlanetURL(e.target.options[e.target.selectedIndex].getAttribute('data-url'))
+  }
+
+  function Planetas() {
+    return (
+      <select onChange={planetChanged} value={selectedPlanet}>
+        <option hidden></option>
+        {planets}
+      </select>
+    );
+  }
+
+  function Residentes() {
+    return (
+      <p>{resNames.toString()}</p>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Planetas Star Wars</h1>
+        <Planetas />
+        <Residentes />
+      </div>
     </div>
   );
 }
